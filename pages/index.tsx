6c7ad371e1fa4ca7864d65e212ccb31d5c3562b8
Zak Auerbach <zak@zaksoup.com>
2020-10-03 04:02:54 +0000

    fix the broken link

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1 className={styles.header}>Welcome to Queerantine!</h1>
     <ul>
-      <li><a href="dnd.queerantine.online">the DND Game</a></li>
+      <li><a href="https://dnd.queerantine.online">the DND Game</a></li>
     </ul>
   </Layout>
 )
