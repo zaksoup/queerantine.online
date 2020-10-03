f38370d23f358dab000299bf7c3242ba5fad3ae6
Zak Auerbach <zak@zaksoup.com>
2020-10-03 04:00:31 +0000

    Now with more links!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Queerantine Online
 ## A homepage for Zak's Queerantine projects
+And now with more links!
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import styles from './index.module.scss'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1 className={styles.header}>Welcome to Queerantine!</h1>
+    <ul>
+      <li><a href="dnd.queerantine.online">the DND Game</a></li>
+    </ul>
   </Layout>
 )
 
